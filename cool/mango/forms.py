60f2ecbc1a4ace4ab332963a4a888e27fffb1518
VladVladVladbot <95708823+VladVladVladbot@users.noreply.github.com>
2022-12-27 17:52:08 +0300

    translate

--- cool/mango/forms.py
+++ cool/mango/forms.py
@@ -3,7 +3,7 @@ from .service import currency
 
 
 class CurrencyForm(forms.Form):
-    count = forms.FloatField(label="Количество")
+    count = forms.FloatField(label="Quantity")
     count_1 = forms.ChoiceField(choices=currency, label="Give")
     count_2 = forms.ChoiceField(choices=currency, label="Get")
 
